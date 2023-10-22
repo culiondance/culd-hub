3ea0fb55b6370a19ef756414385daf01bfb206ec
Mara Dominguez <142527768+ddmngz@users.noreply.github.com>
2023-10-22 19:57:06 -0400

    Modified to work with fly

--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 backend/manage.py makemigrations --no-input
 python3 backend/manage.py migrate --no-input
-#python3 backend/manage.py createsuperuser --noinput --email "drd2145@columbia.edu" --first_name "Amira" --last_name "Dominguez"
+#python3 backend/manage.py createsuperuser --noinput --email "drd2145@columbia.edu" --first_name "Mara" --last_name "Dominguez"
 python3 backend/manage.py runserver 0.0.0.0:8080
