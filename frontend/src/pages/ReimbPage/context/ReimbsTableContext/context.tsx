066faecf816fb84de3b797eb21a9dc67f790e265
Mara Dominguez <maradominguez445@gmail.com>
2024-09-08 14:32:22 -0400

    new query

--- frontend/src/pages/ReimbPage/context/ReimbsTableContext/context.tsx
+++ frontend/src/pages/ReimbPage/context/ReimbsTableContext/context.tsx
@@ -1,14 +1,16 @@
 import React, {createContext, useContext, useEffect, useState} from "react";
 
+import {gql} from "@apollo/client";
 const ReimbsTableContext = createContext(undefined);
 
 export default ReimbsTableContext;
 
 
+// should just need to query my_reimbs from the server
 export const ReimbsTableProvider: React.FC<props> = ({children} : Props) => {
     // query to get list of reimbursements
     // return list + whether or not we should refresh
 }
 
 
-export const GET_REIMBS = gql``;
+
